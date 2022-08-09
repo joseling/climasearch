2da74b22b0b25b1a454f9d0e038f8fa9e4404a61
Josseling Hurtado <64708205+joseling@users.noreply.github.com>
2022-08-09 18:59:41 +0200

    Add files via upload

---
+++ src/components/weather/weatherForm.js
@@ -0,0 +1,36 @@
+import { useState } from "react";
+
+import styles from "./weatherForm.module.css";
+
+export default function WeatherForm({ onChangeCity }) {
+  const [city, setCity] = useState("");
+  
+ // function  onChange(e){
+    //const value = e.target.value;
+   /// if(value ≠ ""){
+    //setCity(value);
+   //}
+  //}
+
+  function handleChange(e) {
+    setCity(e.target.value);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!city || city !== "") {
+      onChangeCity(city);
+    }
+  }
+
+  return (
+    <form onSubmit={handleSubmit} className={styles.container}>
+      <input
+        className={styles.input}
+        type="text"
+        value={city}
+        onChange={handleChange}
+      />
+    </form>
+  );
+}
